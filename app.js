39485a32eb3b43be1aa65b020692a51d1b52cf60
Gideon <gakilas@yahoo.com>
2019-10-19 11:03:58 -0500

    Redirect to private link

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express'),
   app = express();
+  const request = require('request');
 
+
+    app.get('/',(req, res) => {
+        res.render("pages/private_home_page.html");
+    })
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
