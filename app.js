3b02e405e52e1c801a875652a5fdef753a298594
vyas0189 <vyas01890@outlook.com>
2019-10-19 09:41:46 -0500

    added private pages

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
-app.get('/', (req, res)=>{
-    res.render('index.html');
-=======
 app.get('/', (req, res) => {
-  res.send('Hello World');
->>>>>>> 875633124ac84da757f5d98a93cbd4bd5d459847
+  res.render('index.html');
 });
 
-app.listen(process.env.port || 3000);
-<<<<<<< HEAD
+app.listen(process.env.PORT || 3000);
 console.log(`Server Live `);
-=======
-console.log('Server Live on 3000');
->>>>>>> 875633124ac84da757f5d98a93cbd4bd5d459847
--- auth.js
+++ auth.js
@@ -1,25 +1,24 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-module.exports = (passport) => {
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.use(new GoogleStrategy({
-<<<<<<< HEAD
-            clientID: process.env.CLIENTID,
-            clientSecret: process.env.CLIENTSECRET,
-=======
-            clientID: "",
-            clientSecret: "",
->>>>>>> 2bc6ff5a3f57c60a31882f6cf7a45450c3c55f89
-            callbackURL: "http://localhost:3000"
-        },
-        (token, refreshToken, profile, done) => {
-            return done(null, {
-                profile: profile,
-                token: token
-            });
-        }));
+module.exports = passport => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENTID,
+        clientSecret: process.env.CLIENTSECRET,
+        callbackURL: 'http://localhost:3000',
+      },
+      (token, refreshToken, profile, done) => {
+        return done(null, {
+          profile: profile,
+          token: token,
+        });
+      },
+    ),
+  );
 };
