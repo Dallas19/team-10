290b5adb84b4a547e84e821e4917c20f93a006a8
Gideon <gakilas@yahoo.com>
2019-10-19 02:19:03 -0500

    update node redirect

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send("Hello World");
+    res.render('index.html');
 });
 
 
 
 
 app.listen(process.env.port || 3000);
-console.log("Server Live on 3000");
+console.log(`Server Live `);
