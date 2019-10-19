f291b7eb51116ffb8c90814814bc087b867ae3c1
vyas0189 <vyas01890@outlook.com>
2019-10-19 09:46:58 -0500

    added private pages

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express'),
-    app = express();
-
+  app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
-
